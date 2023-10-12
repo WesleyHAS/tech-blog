69964039bb1eaf780d3dba403492a9d1245dcd58
Wes <wesley.has@outlook.com>
2023-10-12 12:04:17 -0600

    setup db connection

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// Setup connection with the database
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+    logging: false,
+  },
+  console.log(`Connected to the blog_db database.`)
+);
+
+module.exports = sequelize;
