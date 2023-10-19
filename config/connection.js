fd2cf2027c4bd6d5f71b20c5472ff2871c7faec2
Wes <wesley.has@outlook.com>
2023-10-19 16:26:47 -0600

    added JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,22 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 const secretKey = process.env.SESSION_SECRET;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-    logging: false,
-  },
-  console.log(`Connected to the blog_db database.`)
-);
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+      logging: false,
+    },
+    console.log(`Connected to the blog_db database.`)
+  );
+}
 
 module.exports = { sequelize, secretKey };
